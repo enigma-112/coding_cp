bbd84f4345b5cd0df61d30a5cfb650acfd771ece
engima-112 <rrd51510@gmail.com>
2021-10-19 04:32:30 +0530

    cp

--- practice/c++/p1.cpp
+++ practice/c++/p1.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
-using namespace std 
+using namespace std ;
 #define ll long long int
 #define endl '\n'
 
 int main(){
-	
+	queue<int> q1;
+	for(int i=1;i<=5;i++){
+		q1.push(i);
+	}
+
+	cout<<"front : "<<q1.front()<<endl;
+	cout<<"back : "<<q1.back();
 }
