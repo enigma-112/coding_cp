bd066374cca52e6c79bcd04dfedb6e571c6dc6bd
Kundan Kumar <rrd51510@gmail.com>
2023-12-31 16:49:30 +0530

    refactoring code

--- basic codes/cpp/cp_structure_small.cpp
+++ basic codes/cpp/cp_structure_small.cpp
@@ -7,17 +7,17 @@ using namespace std;
 #define endl 			"\n"
 #define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-void customio(){
+void customio() {
     #ifndef ONLINE_JUDGE	
 	freopen("D:/coding_cp/input.txt","r",stdin);
 	freopen("D:/coding_cp/output.txt","w",stdout);
     #endif
-}
+}  
 
-int main(){
+int main() {
 	customio();
 	FIO;
-
-
+	cout<<"Hello World !!";
+	cout<<"aaaaaaaaaaaaaaaa";
 	return 0;
 }
